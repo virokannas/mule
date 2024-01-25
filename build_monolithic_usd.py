ee5fed8688f0ba6ab9268f9b0d8206a0de49483d
Simo Virokannas <virokannas@gmail.com>
2024-01-25 15:59:15 -0500

    Update build scripts

--- build_monolithic_usd.py
+++ build_monolithic_usd.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 print("Cloning USD repository...")
 os.system("git clone https://github.com/PixarAnimationStudios/USD .USD_build")
+os.system("./patch_usd_build.sh")
 print("Building a monolithic USD library...")
-os.system("python .USD_build/build_scripts/build_usd.py --build-args TBB,extra_inc=big_iron.inc --no-python --no-tests --no-materialx --no-imaging --no-usdview --no-docs --build-monolithic USDMonolithic")
+os.system("PATH=/Applications/CMake.app/Contents/bin:$PATH python3 .USD_build/build_scripts/build_usd.py --build-args TBB,extra_inc=big_iron.inc --no-python --no-tests --no-materialx --no-imaging --no-usdview --no-docs --build-monolithic USDMonolithic")
 print("Done.")
---
+++ patch_usd_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd .USD_build
+patch -p1 <../build_script_patch.patch
